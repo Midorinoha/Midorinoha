6b7a34b2131a4d5d4551bb7346d964610e7aa76c
Elina Nikitina <101708690+Midorinoha@users.noreply.github.com>
2022-07-18 10:12:59 +0300

    Update DecoratorWithParameters.py

--- DecoratorWithParameters.py
+++ DecoratorWithParameters.py
@@ -1,4 +1,18 @@
 import time
+"""
+Задача на декоратор с параметрами. 
+Надо написать декоратор для повторного выполнения декорируемой функции через некоторое время. Использует наивный экспоненциальный рост времени повтора (factor) до граничного времени ожидания (border_sleep_time).
+В качестве параметров декоратор будет получать:
+
+call_count - число, описывающее кол-во раз запуска функций;
+start_sleep_time - начальное время повтора;
+factor - во сколько раз нужно увеличить время ожидания;
+border_sleep_time - граничное время ожидания.
+Формула:
+
+t = start_sleep_time * factor^(n) if t < border_sleep_time
+t = border_sleep_time if t >= border_sleep_time
+"""
 
 def retry(call_count = 3, start_sleep_time = 1, factor = 2, border_sleep_time = 10):
   def decorator(function):
